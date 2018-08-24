c9b390cc8fec6aa3d15a1d0a80375f008951b077
Matt <voorhees.matthew@gmail.com>
2018-08-24 14:31:56 -0700

    Complete 00_tree_node.rb PolyTreeNode classes

--- Project/skeleton/lib/00_tree_node.rb
+++ Project/skeleton/lib/00_tree_node.rb
@@ -24,11 +24,30 @@ class PolyTreeNode
   end
   
   def remove_child(child_node)
-    
+    raise "Node is not a child" if child_node.parent == nil
+    child_node.parent = nil
   end
   
   def inspect 
     "Node: #{@value}, Parent: #{@parent.value} Children: #{@children.map {|child| child.value}}"
   end
   
+  def dfs(target_value)
+    return self if self.value == target_value
+    self.children.each do |child|
+      search_result = child.dfs(target_value)
+      return search_result unless search_result.nil?
+    end
+    nil
+  end
+  
+  def bfs(target_value)
+    queue = [self]
+    until queue.empty?
+      check_el = queue.shift
+      return check_el if check_el.value == target_value
+      queue += check_el.children
+    end
+    nil
+  end
 end
